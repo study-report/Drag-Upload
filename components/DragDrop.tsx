bcf2668cde1069bb19d5fe15c6f135887bc4b21c
J1min <aoiharu41@gmail.com>
2023-03-20 19:19:11 +0900

    Feat: hooks로 분리

--- components/DragDrop.tsx
+++ components/DragDrop.tsx
@@ -1,153 +1,25 @@
-import React, {
-  ChangeEvent,
-  useCallback,
-  useRef,
-  useState,
-  useEffect,
-} from "react";
+import useFileDrop from "@/hooks/useFileDrop";
+import React, { useEffect } from "react";
 
-interface IFileTypes {
-  id: number;
-  object: File;
-}
-
-const DragDrop = () => {
-  const [isDragging, setIsDragging] = useState<boolean>(false);
-  const [files, setFiles] = useState<IFileTypes[]>([]);
-
-  const dragRef = useRef<HTMLLabelElement | null>(null);
-  const fileId = useRef<number>(0);
-
-  const onChangeFiles = useCallback(
-    (e: ChangeEvent<HTMLInputElement> | any): void => {
-      let selectFiles: File[] = [];
-      let tempFiles: IFileTypes[] = files;
-
-      if (e.type === "drop") {
-        selectFiles = e.dataTransfer.files;
-      } else {
-        selectFiles = e.target.files;
-      }
-
-      for (const file of selectFiles) {
-        tempFiles = [
-          ...tempFiles,
-          {
-            id: fileId.current++,
-            object: file,
-          },
-        ];
-      }
-
-      setFiles(tempFiles);
-    },
-    [files]
-  );
-
-  const handleFilterFile = useCallback(
-    (id: number): void => {
-      setFiles(files.filter((file: IFileTypes) => file.id !== id));
-    },
-    [files]
-  );
-
-  const handleDragIn = useCallback((e: DragEvent): void => {
-    e.preventDefault();
-    e.stopPropagation();
-  }, []);
-
-  const handleDragOut = useCallback((e: DragEvent): void => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    setIsDragging(false);
-  }, []);
-
-  const handleDragOver = useCallback((e: DragEvent): void => {
-    e.preventDefault();
-    e.stopPropagation();
-
-    if (e.dataTransfer!.files) {
-      setIsDragging(true);
-    }
-  }, []);
-
-  const handleDrop = useCallback(
-    (e: DragEvent): void => {
-      e.preventDefault();
-      e.stopPropagation();
-
-      onChangeFiles(e);
-      setIsDragging(false);
-    },
-    [onChangeFiles]
-  );
-
-  const initDragEvents = useCallback((): void => {
-    if (dragRef.current !== null) {
-      dragRef.current.addEventListener("dragenter", handleDragIn);
-      dragRef.current.addEventListener("dragleave", handleDragOut);
-      dragRef.current.addEventListener("dragover", handleDragOver);
-      dragRef.current.addEventListener("drop", handleDrop);
-    }
-  }, [handleDragIn, handleDragOut, handleDragOver, handleDrop]);
-
-  const resetDragEvents = useCallback((): void => {
-    if (dragRef.current !== null) {
-      dragRef.current.removeEventListener("dragenter", handleDragIn);
-      dragRef.current.removeEventListener("dragleave", handleDragOut);
-      dragRef.current.removeEventListener("dragover", handleDragOver);
-      dragRef.current.removeEventListener("drop", handleDrop);
-    }
-  }, [handleDragIn, handleDragOut, handleDragOver, handleDrop]);
+const App = () => {
+  const { inputRef, labelRef, files, isDragActive } = useFileDrop();
 
   useEffect(() => {
-    initDragEvents();
-
-    return () => resetDragEvents();
-  }, [initDragEvents, resetDragEvents]);
+    console.log(files);
+  }, [files]);
 
   return (
-    <div className="DragDrop">
-      <input
-        type="file"
-        id="fileUpload"
-        style={{ display: "none" }}
-        multiple={true}
-        onChange={onChangeFiles}
-      />
-
-      <label
-        className={isDragging ? "DragDrop-File-Dragging" : "DragDrop-File"}
-        htmlFor="fileUpload"
-        ref={dragRef}
-      >
-        <div>파일 첨부</div>
+    <div>
+      <input ref={inputRef} id="upload" />
+      <label ref={labelRef} htmlFor="upload">
+        {isDragActive ? (
+          <span>Drop the file!</span>
+        ) : (
+          <span>Drag and drop the file.</span>
+        )}
       </label>
-
-      <div className="DragDrop-Files">
-        {files.length > 0 &&
-          files.map((file: IFileTypes) => {
-            const {
-              id,
-              object: { name },
-            } = file;
-
-            return (
-              <div key={id}>
-                <div>{name}</div>
-                <div
-                  className="DragDrop-Files-Filter"
-                  onClick={() => handleFilterFile(id)}
-                >
-                  X
-                </div>
-              </div>
-            );
-          })}
-      </div>
     </div>
   );
 };
 
-export default DragDrop;
+export default App;
